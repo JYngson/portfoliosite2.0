0bcefb8407157664e7a0b229ab0dd6cc71c828be
tjarevalo <jimmyjamesyngson@gmail.com>
2023-03-13 19:47:03 -0700

    edited tailwind config again

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,8 @@ module.exports = {
     },
   },
   purge: {
-    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+    enabled:true,
+    content: ["./src/pages//*.{js,jsx,ts,tsx}", "./src/components//*.{js,jsx,ts,tsx}"]
   },
 
   plugins: [],
